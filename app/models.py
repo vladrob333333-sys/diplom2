8795aa80b32c3e17811b89399a13836a87cf35dc
vladrob333333-sys <vladrob333333@gmail.com>
2026-03-22 16:36:03 +0300

    models.py

--- app/models.py
+++ app/models.py
@@ -8,8 +8,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-    role = db.Column(db.String(20), nullable=False)  # admin, operator, executor
+    password_hash = db.Column(db.String(256))  # изменено с 128 на 256
+    role = db.Column(db.String(20), nullable=False)
     is_active = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
